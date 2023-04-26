46dfe536437bc2f7268a222232473a54df1ee68b
helloshiv01 <shiv07221202021@msijanakpuri.com>
2023-04-26 19:50:59 +0530

    react cad

---
+++ src/Experience.js
@@ -0,0 +1,51 @@
+import { Sparkles, OrbitControls, Environment, useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "react-three-fiber";
+
+export default function Experience() {
+  const baseplate1 = useGLTF("./base-plate-1.gltf");
+  const baseplate2 = useGLTF("./base-plate-2.gltf");
+  const bbsplicebolted = useGLTF("./bb-splice-bolted.gltf");
+  const bcep = useGLTF("./bc-ep.gltf");
+  const finplate = useGLTF("./Fin-plate.gltf");
+  const tensionmember = useGLTF("./tension-member.gltf");
+  const baseplate1Ref = useRef();
+  const baseplate2Ref = useRef();
+  const bbspliceboltedRef = useRef();
+  const bcepRef = useRef();
+  const finplateRef = useRef();
+  const tensionmemberRef = useRef();
+
+  useFrame((state, delta) => {
+    baseplate1Ref.current.rotation.y += delta * 0.5; 
+    baseplate2Ref.current.rotation.y += delta * 0.3; 
+    bbspliceboltedRef.current.rotation.x += delta * 0.8;
+    bcepRef.current.rotation.y += delta * -0.2; 
+    finplateRef.current.rotation.z += delta * 2; 
+    tensionmemberRef.current.rotation.x += delta * 1; 
+  });
+
+  return (
+    <>
+      <OrbitControls />
+      
+      <primitive object={baseplate1.scene} position={[2, 0, 0]} ref={baseplate1Ref} />
+      <primitive object={baseplate2.scene} position={[-2, 0, 0]} ref={baseplate2Ref} />
+      <primitive object={bbsplicebolted.scene} position={[0, 2, 0]} ref={bbspliceboltedRef} />
+      <primitive object={bcep.scene} position={[0, 0, 2]} ref={bcepRef} />
+      <primitive object={finplate.scene} position={[4, 0, 0]} ref={finplateRef} />
+      <primitive object={tensionmember.scene} position={[0, -2, 0]} ref={tensionmemberRef} />
+
+      <Sparkles
+        size={0.5}
+        scale={[-1, 1, 1]}
+        position-x={-0}
+        position-y={-0.5}
+        position-z={1}
+        speed={1}
+        count={30}
+      />
+      <Environment preset="city" />
+    </>
+  );
+}
