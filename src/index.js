b9dd2018f991709a782f62f6b1f9c6ee98a87ca9
Shiv Chaudhary <104753095+helloshiv01@users.noreply.github.com>
2023-05-19 17:34:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from './Experience'
+import BasicExample from './components/BasicExample'
+import PlacementExample from "./components/PlacementExample";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<Canvas>
+root.render(<><Canvas>
 <Experience />
-</Canvas>);
+</Canvas>
+<BasicExample />
+<PlacementExample />
+</>
+);
